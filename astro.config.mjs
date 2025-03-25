d97475189c83e3010dcaa9fc62d2da8159b05bdf
stillamusic <stillamusic@gmail.com>
2025-03-25 00:02:56 -0600

    github deploy before dns config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stillamusic.com',
+  site: 'https://stillamusic.github.io',
 });
